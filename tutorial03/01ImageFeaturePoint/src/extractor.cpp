a7559cd450e2db6b8a1d61dfe6ee468a80ef65a3
dengqi <dengqi935@gmail.com>
2022-10-10 23:28:05 +0800

    t03-01: add sift and harries2d

--- tutorial03/01ImageFeaturePoint/src/extractor.cpp
+++ tutorial03/01ImageFeaturePoint/src/extractor.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 int main(int argc, char* argv[]) {
-  cv::Mat image = cv::imread(argv[1]);
+  cv::Mat image = cv::imread(argv[1], 0);
 
-  cv::imshow("Image", image);
+  cv::imshow("Origin Image", image);
   cv::waitKey(0);
+
+
+  cv::Ptr<cv::SIFT> siftPtr = cv::SIFT::create();
+  std::vector<cv::KeyPoint> sift_keypoints;
+  siftPtr->detect(image, sift_keypoints);
+
+  std::cout << "SIFT feature detector detect " << sift_keypoints.size() << " points" << std::endl;
+
+
+  cv::Mat output;
+  cv::drawKeypoints(image, sift_keypoints, output);
+  cv::imshow("Image with SIFT feature points", output);
+  cv::waitKey(0);
+
+  const int block_size = 2;
+  const int aperture_size = 3;
+  const double k = 0.04;
+  const int thresh = 200;
+  std::vector<cv::KeyPoint> harris_keypoints;
+  cv::Mat harris_dst = cv::Mat::zeros(image.size(), CV_32FC1);
+  cv::cornerHarris(image, harris_dst, block_size, aperture_size, k); 
+  cv::Mat harris_dst_norm, harris_dst_norm_scaled;
+  cv::normalize(harris_dst, harris_dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+  cv::convertScaleAbs(harris_dst_norm, harris_dst_norm_scaled);
+  for(int i = 0; i < harris_dst_norm.rows; i++){
+    for(int j = 0; j < harris_dst_norm.cols; j++){
+      if(static_cast<int>(harris_dst_norm.at<float>(i, j)) > thresh)
+      {
+         cv::circle(harris_dst_norm_scaled, cv::Point(j, i), 5, cv::Scalar(0), 2, 8, 0);
+         harris_keypoints.emplace_back(cv::Point(j, i), 5);
+      }
+    }
+  }
+  std::cout << "harris 2d detect " << harris_keypoints.size() << " corner points" << std::endl;
+  cv::imshow("Image with harris2d corner point", harris_dst_norm_scaled);
+  cv::waitKey(0);
+
+
+
+  return 0;
 }
