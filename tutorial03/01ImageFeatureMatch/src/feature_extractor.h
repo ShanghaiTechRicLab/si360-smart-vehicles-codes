0bef880c493c78221e11a86833396769660a0cc5
dengqi <dengqi935@gmail.com>
2022-10-12 16:40:08 +0800

    t03-01: format the code

--- tutorial03/01ImageFeatureMatch/src/feature_extractor.h
+++ tutorial03/01ImageFeatureMatch/src/feature_extractor.h
@@ -10,7 +10,7 @@
 #include "simple_timer.hpp"
 
 class FeatureExtractor {
-public:
+ public:
   FeatureExtractor() = default;
   FeatureExtractor(const std::string &name);
   virtual ~FeatureExtractor();
@@ -23,41 +23,41 @@ public:
   virtual void detect(const cv::Mat &image,
                       std::vector<cv::KeyPoint> &keypoints);
 
-protected:
+ protected:
   std::string name_;
   std::unique_ptr<StopWatchTimer> timer_;
 };
 
 class SiftFeatureExtractor : FeatureExtractor {
-public:
+ public:
   SiftFeatureExtractor();
   ~SiftFeatureExtractor();
   void detect(const cv::Mat &image,
               std::vector<cv::KeyPoint> &keypoints) override;
 
-public:
+ public:
   cv::Ptr<cv::SIFT> sift_ptr_;
 };
 
 class SurfFeatureExtractor : FeatureExtractor {
-public:
+ public:
   SurfFeatureExtractor();
   ~SurfFeatureExtractor();
   void detect(const cv::Mat &image,
               std::vector<cv::KeyPoint> &keypoints) override;
 
-public:
+ public:
   cv::Ptr<cv::xfeatures2d::SURF> surf_ptr_;
 };
 
 class FastFeatureExtractor : FeatureExtractor {
-public:
+ public:
   FastFeatureExtractor();
   ~FastFeatureExtractor();
   void detect(const cv::Mat &image,
               std::vector<cv::KeyPoint> &keypoints) override;
 
-public:
+ public:
   cv::Ptr<cv::FeatureDetector> fast_ptr_;
 };
 
